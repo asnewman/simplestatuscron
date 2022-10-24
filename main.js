83b20cffb97423c12d768deb67c0055fa01806a5
asnewman <ashznewman@gmail.com>
2022-10-23 18:24:37 -0700

    Use deno cron check

--- main.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios").default;
-
-axios.post("https://simplestat.us/api/check").then(() => {
-  console.log("Success!");
-}).catch((err) => {
-  console.error(err);
-}
---
+++ main.ts
@@ -0,0 +1,1 @@
+fetch("https://simplestat.us/api/check", {method: "POST"})
