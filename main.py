dba908d360d9b5f11dd537f11625f1bd4132af03
asnewman <ashznewman@gmail.com>
2022-10-23 18:48:49 -0700

    typo

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from urllib import request
 import requests
 
 def run():
-  request.post("https://simplestat.us/api/check")
+  requests.post("https://simplestat.us/api/check")
 
 if __name__ == "__main__":
   run()
