7019956fe645ca00b2e6f808c6532a91646ab436
asnewman <ashznewman@gmail.com>
2022-11-14 13:52:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 
 def run():
   requests.post("https://simplestat.us/api/check")
-  requests.post("https://simplestat.us/api/checkManagedDependencies")
+  #requests.post("https://simplestat.us/api/checkManagedDependencies")
 
 if __name__ == "__main__":
   run()
